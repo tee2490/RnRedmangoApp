ace87c7669f3b0aa3ed24f68b0f1fdfa2da97354
Teeradet <tee2490@hotmail.com>
2024-09-30 16:14:43 +0700

    Section 02 : Add Reducers to Shopping Cart Slice

--- src/redux/shoppingCartSlice.ts
+++ src/redux/shoppingCartSlice.ts
@@ -1,18 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { shoppingCartModel } from "../interfaces";
 
-const initialState = {
-    cartItems: [],
+const initialState: shoppingCartModel = {
+  cartItems: [],
 };
 
 export const shoppingCartSlice = createSlice({
-    name: "cartItems",
-    initialState: initialState,
-    reducers: {
-        setShoppingCart: (state, action) => {
-            state.cartItems = action.payload;
-        },
+  name: "cartItems",
+  initialState: initialState,
+  reducers: {
+    setShoppingCart: (state, action) => {
+      state.cartItems = action.payload;
     },
+    updateQuantity: (state, action) => {
+      //payload - cart item that needs to be updated, newquantity
+      state.cartItems = state.cartItems?.map((item) => {
+        if (item.id === action.payload.cartItem.id) {
+          item.quantity = action.payload.quantity;
+        }
+        return item;
+      });
+    },
+    removeFromCart: (state, action) => {
+      //payload - cart item that needs to be updated, newquantity
+      state.cartItems = state.cartItems?.filter((item) => {
+        if (item.id === action.payload.cartItem.id) {
+          return null;
+        }
+        return item;
+      });
+    },
+  },
 });
 
-export const { setShoppingCart } = shoppingCartSlice.actions;
+export const { setShoppingCart, updateQuantity, removeFromCart } =
+  shoppingCartSlice.actions;
 export const shoppingCartReducer = shoppingCartSlice.reducer;

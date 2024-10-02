7b9fa70e83159716b734d2f365c9b39660bb1028
Teeradet <tee2490@hotmail.com>
2024-10-02 21:26:06 +0700

    Section 04 : Prepare Stripe Card Demo

--- src/screen/PaymentScreen.tsx
+++ src/screen/PaymentScreen.tsx
@@ -1,16 +1,29 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../navigates/typeRootStack";
+import { StripeProvider } from "@stripe/stripe-react-native";
+import { PaymentForm } from "../components/payment";
 
 type Props = NativeStackScreenProps<RootStackParamList, "PaymentScreen">;
 
 export default function PaymentScreen({ route }: Props) {
   const { state } = route.params;
+  const [publishableKey, setPublishableKey] = useState("");
+  
+  const fetchPublishableKey = async () => {
+    const key =
+      "pk_test_51M9JKILEJFIvBBF2XkujZkrsfTEzCwhb6Mju4cg46E92bFLupZh7FxanSKa17WqSqpfl3WuQ1K3AL2VbK1wCrg9200hagD6XvF";
+    setPublishableKey(key);
+  };
+
+  useEffect(() => {
+    fetchPublishableKey();
+  }, []);
 
   return (
-    <View>
-      <Text>{state?.apiResult}</Text>
-    </View>
+    <StripeProvider publishableKey={publishableKey}>
+      <PaymentForm />
+    </StripeProvider>
   );
 }

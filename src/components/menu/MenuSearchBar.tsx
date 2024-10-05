b155e94d5753873a0457a46b9df76b8ee9f2a4b5
Teeradet <tee2490@hotmail.com>
2024-10-05 17:19:55 +0700

    Section 07 : Design SearchBar

---
+++ src/components/menu/MenuSearchBar.tsx
@@ -0,0 +1,54 @@
+import { View, StyleSheet, TextInput } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { debounce } from "lodash";
+import { useCallback, useEffect, useState } from "react";
+import { COLORS } from "../../common";
+
+export default function MenuSearchBar() {
+  const [debouncedValue, setDebouncedValue] = useState<string>("");
+
+  // Create a debounced function
+  const debouncedSearch = useCallback(
+    debounce((text: string) => {
+      setDebouncedValue(text);
+    }, 1000), // 1000 ms debounce time
+    []
+  );
+
+  // Handle input change
+  const handleChange = (text: string) => {
+    debouncedSearch(text);
+  };
+
+  useEffect(() => {
+    console.log(debouncedValue);
+  }, [debouncedValue]);
+
+  return (
+    <View style={styles.container}>
+      <AntDesign name="search1" size={20} color={COLORS.primary2} />
+      <TextInput
+        placeholder="Search here..."
+        style={styles.textInput}
+        onChangeText={handleChange}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: COLORS.primary2,
+    padding: 10,
+  },
+  textInput: {
+    paddingLeft: 10,
+    flex: 1,
+    color: COLORS.primary2,
+    fontSize: 18,
+  },
+});
--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -1,12 +1,30 @@
-import { View } from 'react-native'
-import React from 'react'
-import { MenuItemList } from '../components/home'
+import { View, StyleSheet } from "react-native";
+import React from "react";
+import { MenuItemList } from "../components/home";
+import { MenuSearchBar } from "../components/menu";
+import { COLORS, SIZES } from "../common";
 
 export default function HomeScreen() {
   return (
     <View>
+      <View style={styles.searchContainer}>
+        <MenuSearchBar />
+      </View>
       <MenuItemList />
     </View>
-  )
+  );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingLeft: SIZES.small / 2,
+    backgroundColor: COLORS.white,
+  },
+  searchContainer: {
+    margin: SIZES.xSmall,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
